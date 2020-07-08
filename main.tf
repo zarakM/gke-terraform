56f4b1658344e490648c5fb4e76e0ca51c877005
zarakM <mughalzarak@gmail.com>
2020-07-08 20:33:22 +0500

    integration od VPC to isolate gke

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+# VPC integration
+resource "google_compute_network" "vpc" {
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+
+}
